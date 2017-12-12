798e435977bb6ce05a2f984fb19573985adcd797
Mareise <34275515+Mareise@users.noreply.github.com>
2017-12-12 16:32:07 +0100

    grias de

--- Server.js
+++ Server.js
@@ -1,20 +1,22 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
 app.use(express.static('public'))
-app.listen(3000,function() {
-console.log('listening on port 3000')
+app.listen(3000, function () {
+	console.log('listening on port 3000')
 })
 
 app.get('/hello', function (req, res) {
-res.send('Hello World!')
+	res.send('Hello World!')
 })
 
 
 app.get('/api/students/:studentId',
-function (req,res) {
-	if(req.params.studentId==1234){
-	res.status(404).send('Uuups!')}
-	else {
-	res.send('Hallo');}
-}
-})
+	function (req, res) {
+		if (req.params.studentId == 1234) {
+			res.status(404).send('Uuups!')
+		}
+		else {
+			res.send('Hallo');
+		}
+	}
+)
