6c94ec67c73d150de429140af5042f104b3ec775
Mareise <34275515+Mareise@users.noreply.github.com>
2018-01-16 15:22:11 +0100

    Query

---
+++ Public/StartPage_script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+
+    console.log('Hallo');
+
+    document.getElementById("Success").style.display = "";
+
+    function updateQueryStringParameter(uri, key, value) {
+            return "uri" + "separator" + "key" + "=" + "value";
+
+    }
+
+};
