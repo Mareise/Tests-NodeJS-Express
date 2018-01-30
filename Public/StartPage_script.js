7cc0ed03de8c924856c362bbe5d3468db63ea839
Mareise <34275515+Mareise@users.noreply.github.com>
2018-01-30 09:20:27 +0100

    Tests AJAX

---
+++ Public/StartPage_script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    console.log("Seas")
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "/Test/id/:Id", true);
+    xhttp.send();
+}
