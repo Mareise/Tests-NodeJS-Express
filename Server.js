ea6a5fffe0c2556babbf8c6075ea6c3283111ce5
Mareise <34275515+Mareise@users.noreply.github.com>
2018-01-30 14:40:13 +0100

    Schleife

--- Server.js
+++ Server.js
@@ -12,73 +12,74 @@ app.get('/hello', function (req, res) {
 })
 
 
+while (true) {
+
+	app.get('/Test/id/:Id',
+		function (req, res) {
+			var dutycycle = parseInt(req.params.Id);
+			console.log(dutycycle);
+			if (dutycycle >= 0) {
+
+				var i2cBus = require("i2c-bus");
+				var Pca9685Driver = require("pca9685").Pca9685Driver;
+				var http = require('http');
+
+				var http = require('http'),
+					fs = require('fs');
 
 
-app.get('/Test/id/:Id',
-	function (req, res) {
-		var dutycycle = parseInt(req.params.Id);
-		console.log(dutycycle);
-		if (dutycycle >= 0) {
-
-			var i2cBus = require("i2c-bus");
-			var Pca9685Driver = require("pca9685").Pca9685Driver;
-			var http = require('http');
-
-			var http = require('http'),
-				fs = require('fs');
-
-
-			fs.readFile('./LongdrinkPage.html', function (err, html) {
-				if (err) {
-					throw err;
-				}
-				http.createServer(function (request, response) {
-					response.writeHeader(200, { "Content-Type": "text/html" });
-					response.write(html);
-					response.end();
-				}).listen(8000);
-			});
-
-			res.send('grias de')
-
-			var options = {
-				i2c: i2cBus.openSync(1),
-				address: 0x40,
-				frequency: 50,
-				debug: false
-			};
-			console.log("Seas wos geht");
-			pwm = new Pca9685Driver(options, function (err) {
-				if (err) {
-					console.error("Error initializing PCA9685");
-					process.exit(-1);
-				}
-				console.log("Initialization done");
-
-				// Set channel 0 to turn on on step 42 and off on step 255
-				// (with optional callback)
-				pwm.setPulseRange(0, 42, 255, function () {
+				fs.readFile('./LongdrinkPage.html', function (err, html) {
 					if (err) {
-						console.error("Error setting pulse range.");
-					} else {
-						console.log("Pulse range set.");
+						throw err;
 					}
+					http.createServer(function (request, response) {
+						response.writeHeader(200, { "Content-Type": "text/html" });
+						response.write(html);
+						response.end();
+					}).listen(8000);
 				});
 
-				// Set the pulse length to 1500 microseconds for channel 2
-				pwm.setPulseLength(2, 1500);
+				res.send('grias de')
 
-				// Set the duty cycle to 25% for channel 8
-				console.log("Channel 8  done")
-				pwm.setDutyCycle(8, dutycycle);
+				var options = {
+					i2c: i2cBus.openSync(1),
+					address: 0x40,
+					frequency: 50,
+					debug: false
+				};
+				console.log("Seas wos geht");
+				pwm = new Pca9685Driver(options, function (err) {
+					if (err) {
+						console.error("Error initializing PCA9685");
+						process.exit(-1);
+					}
+					console.log("Initialization done");
 
-				// Turn on channel 3 (100% power)
-				pwm.channelOn(3);
-			});
+					// Set channel 0 to turn on on step 42 and off on step 255
+					// (with optional callback)
+					pwm.setPulseRange(0, 42, 255, function () {
+						if (err) {
+							console.error("Error setting pulse range.");
+						} else {
+							console.log("Pulse range set.");
+						}
+					});
 
+					// Set the pulse length to 1500 microseconds for channel 2
+					pwm.setPulseLength(2, 1500);
+
+					// Set the duty cycle to 25% for channel 8
+					console.log("Channel 8  done")
+					pwm.setDutyCycle(8, dutycycle);
+
+					// Turn on channel 3 (100% power)
+					pwm.channelOn(3);
+				});
+
+			}
+			else {
+				res.send('Falsche ID')
+			}
 		}
-		else {
-			res.send('Falsche ID')
-		}
-	}
-)
+	)
+}
