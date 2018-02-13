3b2e3d956e88b1ad4e151cf4f5a0e7a38f0c09f7
mareise <maxi.reisecker@gmail.com>
2018-02-13 12:39:44 +0000

    Fehlersuche

--- Public/StartPage_script.js
+++ Public/StartPage_script.js
@@ -1,11 +1,11 @@
 function myFunction() {
     var Dutycycle = document.getElementById("Dutycycle").value;
-    console.log("Dutycicle" + Dutycycle);
+    console.log("Dutycicle= " + Dutycycle);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log("Respnse:"+this.responseText)
-            document.getElementById("demo").innerHTML = this.responseText;
+        if (this.readyState == 4 || this.status == 200) {
+            console.log("Response: "+this.responseText)
+            document.getElementById("demo").innerHTML = "Success"
         }
     };
 
