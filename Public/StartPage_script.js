d598920b64dbde56df19f64b70ee8bad749483af
Mareise <34275515+Mareise@users.noreply.github.com>
2018-02-16 13:30:59 +0100

    Fehler behoben

--- Public/StartPage_script.js
+++ Public/StartPage_script.js
@@ -3,13 +3,12 @@ function myFunction() {
     console.log("Dutycicle= " + Dutycycle);
     var xhttp = new XMLHttpRequest();
     
-     httpReq.onload = function() {
+     xhttp.onload = function() {
         if(this.status==200) {
             console.log("JUHUUUUUUUUUU")
             document.getElementById('demo').innerHTML=this.status
         } else {
-            let errorTxt = 'Error: '+this.status+' ('+this.statusText+')'
-            document.getElementById('demo').innerHTML=errorTxt
+            document.getElementById('demo').innerHTML="Fehler"
             
         }
     }
