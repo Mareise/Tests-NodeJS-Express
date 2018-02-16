0179faa637d88dca17ec0189cfccc50d04853cf5
Mareise <34275515+Mareise@users.noreply.github.com>
2018-02-16 16:16:37 +0100

    änderungen

--- Public/StartPage_script.js
+++ Public/StartPage_script.js
@@ -2,19 +2,24 @@ function myFunction() {
     var Dutycycle = document.getElementById("Dutycycle").value;
     console.log("Dutycicle= " + Dutycycle);
     var xhttp = new XMLHttpRequest();
+
+    var query = "/Test/id/" + Dutycycle;
+    console.log(query);
+    xhttp.open("GET", query, true);
     
      xhttp.onload = function() {
         if(this.status==200) {
             console.log("JUHUUUUUUUUUU")
             document.getElementById('demo').innerHTML=this.status
         } else {
+            console.log("buuu")
             document.getElementById('demo').innerHTML="Fehler"
-            
         }
     }
+    xhttp.onerror = function () {
+        console.log("Error")
+    }
 
-    var query = "/Test/id/" + Dutycycle;
-    console.log(query);
-    xhttp.open("GET", query, true);
+    
     xhttp.send();
 }
